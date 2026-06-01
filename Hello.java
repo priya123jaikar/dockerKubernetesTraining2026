e4e629358b5958792f7478eb08fce940c502ac7a
priya123jaikar <47687811+priya123jaikar@users.noreply.github.com>
2026-06-01 17:42:27 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-public class Demo {
+public class Hello {
     public static void main(String[] args) {
         System.out.println("Java Demo Program");
         System.out.println("=================");
